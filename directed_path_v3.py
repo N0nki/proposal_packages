78cf6652d45575702ba4de55f0711d9bc8ca7e19
N0nki <rgstr_land@icloud.com>
2016-11-25 14:28:55 +0900

    fix function original_path

--- directed_path_v3.py
+++ directed_path_v3.py
@@ -234,15 +234,13 @@ def original_path(path):
         if isinstance(i, tuple):
             o_edge = (i[0], i[1])
             o_path.append(o_edge)
-            # _path.remove((i[0], i))
             _path.remove((i,j))
-            _path.remove((j,i))
+            _path.remove((i[0], i))
         elif isinstance(j, tuple):
             o_edge = (j[0], j[1])
             o_path.append(o_edge)
-            # _path.remove((j, j[1]))
             _path.remove((i,j))
-            _path.remove((j,i))
+            _path.remove((j, j[1]))
         else:
             o_path.append((i,j))
     return o_path
