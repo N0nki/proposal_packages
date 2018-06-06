8e2cdcfee10f195e86336cd84d313fa5f929b4a5
N0nki <rgstr_land@icloud.com>
2018-06-07 00:29:53 +0900

    update

--- gridgraph.py
+++ gridgraph.py
@@ -3,17 +3,18 @@ import random
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from graphillion import GraphSet
 
 class GridGraph:
 
-    def __init__(self, m, n):
+    def __init__(self, m, n, prob_to_remove_edge=0.0):
+        assert prob_to_remove_edge >= 0 and prob_to_remove_edge < 0.4
+        self.prob_to_remove_edge = prob_to_remove_edge
         self.m = m
         self.n = n
-        self.nx_graph = self._grid_graph(m, n)
+        self.nx_graph = self._grid_graph()
         self.node_pos = self._get_node_pos()
 
-    def _grid_graph(self, m, n):
+    def _grid_graph(self):
         """
         m×nの格子グラフを返す
         """
@@ -35,18 +36,20 @@ class GridGraph:
         ノードの位置座標を返す
         """
         n = sorted(self.nx_graph[1].keys())[1] - 1
-        m = self.nx_graph.number_of_nodes() // self.n
+        m = self.nx_graph.number_of_nodes() // n
         pos = {}
         for v in range(1, m * n + 1):
             pos[v] = ((v - 1) % n, (m * n - v) // n)
         return pos
 
-    def draw(self, subgraph=None, edge_labels=None, figsize=None, optional_node_label=None):
+    def draw(self, subgraph=None, edge_labels=None, optional_node_label=None):
         """
         格子グラフを描画する
 
-        TODO: ノード右上に任意の数字のラベルを表示する機能を追加する．
-              ノードをキー，数字を値とするoptionalな引数optional_node_labelを利用する．
+        arguments:
+        * subgraph(edgelist, optional)
+          指定すると、その部分が赤く表示される
+        * edge_labels(dict, optional)
         """
         nx.draw_networkx_nodes(self.nx_graph, self.node_pos, node_color='w')
         nx.draw_networkx_labels(self.nx_graph, self.node_pos)
@@ -61,7 +64,7 @@ class GridGraph:
                 subgraph_metric = {(e[0],e[1]): edge_labels[e] for e in subgraph.edges()}
                 nx.draw_networkx_edge_labels(subgraph, self.node_pos, edge_labels=subgraph_metric, font_color='r', font_size=8)
         if optional_node_label is not None:
-            for node,num in optional_node_label.values():
-                plt.text(self.node_pos[node][0]+0.1, node_pos[node][1]+0.2, num, fontsize=11)
+            for node, num in optional_node_label.values():
+                plt.text(self.node_pos[node][0]+0.1, self.node_pos[node][1]+0.2, num, fontsize=11)
         plt.xticks([])
         plt.yticks([])
